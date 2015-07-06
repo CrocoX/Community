e685e3c75dce8dde02a2c95c55e5768fa31559bb
CrocoX <crocox@safe-mail.net>
2015-07-06 15:38:20 +0200

    Update Main.java

--- src/de/janhektor/community/Main.java
+++ src/de/janhektor/community/Main.java
@@ -38,7 +38,7 @@ public class Main extends JavaPlugin {
 	@Override
 	public void onDisable() {
 		this.getLogger().log(Level.INFO, "Community plugin version " + this.getDescription().getVersion()
-			+ " by " + this.getDescription().getAuthors().toString() + " disabled!");
+				+ " by " + this.getDescription().getAuthors().toString() + " disabled!");
 	}
 
 
